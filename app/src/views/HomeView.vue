17b8b75f9e1fe97ed8a9d6e4c92072f95a2cf2fd
KenzieLeonic <57589336+KenzieLeonic@users.noreply.github.com>
2022-10-17 13:50:12 +0700

    add bg color

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <template >
-  <div class="inline-flex">
+  <body>
+  <div class="inline-flex h-screen bg-angelBaby-100">
     <div class="font-mono mx-auto max-w-7xl text-gray-700">
       <h1 class="pl-8 pt-6 text-xl">หน้าหลัก</h1>
     </div>
   </div>
-
+  </body>
 </template>
 
 <style scoped>
-
+body{
+  background-color: #E8F0FF;
+}
 </style>
--- app/src/views/InboundView.vue
+++ app/src/views/InboundView.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="inline-flex bg-angelBaby-100">
-      <div class="font-mono mx-auto max-w-7xl text-gray-700">
-        <h1 class="pl-8 pt-6 text-xl">Inbound</h1>
-      </div>
+  <body>
+  <div class="inline-flex h-screen bg-angelBaby-100">
+    <div class="font-mono mx-auto max-w-7xl text-gray-700">
+      <h1 class="pl-8 pt-6 text-xl">Inbound</h1>
     </div>
-  
+  </div>
+  </body>
   </template>
 <script>
 
 </script>
 
 <style scoped>
-  
+body{
+  background-color: #E8F0FF;
+}
 </style>
