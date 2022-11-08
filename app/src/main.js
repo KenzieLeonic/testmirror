1d4d3b79a2330fdb2b0f79206f5dc0ebd21c873f
Ratchathorn <ratchathorn.s@ku.th>
2022-11-08 21:28:16 +0700

    removed auth0

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createAuth0 } from '@auth0/auth0-vue';
 import axios from 'axios'
 
 import App from './App.vue'
@@ -20,11 +19,5 @@ const axiosInstance = axios.create({
 app.config.globalProperties.$axios = { ...axiosInstance }
 app.use(createPinia())
 app.use(router)
-app.use(  createAuth0({
-  domain: "reina.jp.auth0.com",
-  client_id: "jc24Sjqsy5UZNfXedTjFv7aWG17QOySF",
-  redirect_uri: window.location.origin,
-  audience: "https://angel-baby-db/api",
-  }))
 app.mount('#app')
 
