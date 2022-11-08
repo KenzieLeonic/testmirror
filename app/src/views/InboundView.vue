f68a5d38be8ab7a5fb2e4e08a4eba3a5854202a9
aungpor <napat.patc@ku.th>
2022-11-08 11:48:12 +0700

    outbound data

--- app/src/views/InboundView.vue
+++ app/src/views/InboundView.vue
@@ -68,10 +68,10 @@
 
         <div v-if="selected != null" class="grid w-1/2 h-full grid-cols-2 grid-rows-6 gap-6 p-3 my-20 border-2 x-10 bg-angelBaby-200 right-2 ">
           <h3 class="float-left text-xl text-gray-50">
-            Log ID
+            Stock ID
           </h3>
           <h3 class="float-left text-xl text-gray-50">
-            {{selected.logID}}
+            {{selected.stock.stockID}}
           </h3>
           <h3 class="float-left text-xl text-gray-50">
             Item ID
@@ -112,7 +112,7 @@
 
         <div v-else class="grid w-1/2 h-full grid-cols-2 grid-rows-6 gap-6 p-3 my-20 border-2 x-10 bg-angelBaby-200 right-2 ">
           <h3 class="float-left text-xl text-gray-50">
-            Log ID
+            Stock ID
           </h3>
           <h3> </h3>
           <h3 class="float-left text-xl text-gray-50">
